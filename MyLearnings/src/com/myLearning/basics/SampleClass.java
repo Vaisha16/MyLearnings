9f954fc828019654480f53a33e4a37095e61d9c9
Vaisha16 <vaishnavi.saravana01@gmail.com>
2021-09-17 12:32:19 +0530

    New line added

--- MyLearnings/src/com/myLearning/basics/SampleClass.java
+++ MyLearnings/src/com/myLearning/basics/SampleClass.java
@@ -6,6 +6,7 @@ public class SampleClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("to check the changes done in eclipse after committing in github");
+		System.out.println("this is new line");
 	}
 
 }
