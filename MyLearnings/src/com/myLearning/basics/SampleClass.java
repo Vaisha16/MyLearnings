474a299351c9d2179d3c18e81eb05d58b8ed3c0a
Vaisha16 <vaishnavi.saravana01@gmail.com>
2021-09-17 12:30:23 +0530

    to check the changes done in eclipse.

--- MyLearnings/src/com/myLearning/basics/SampleClass.java
+++ MyLearnings/src/com/myLearning/basics/SampleClass.java
@@ -5,6 +5,7 @@ public class SampleClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("to check the changes done in eclipse after committing in github");
 	}
 
 }
