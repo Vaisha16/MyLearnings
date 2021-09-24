22e362fd1fed771241d62512c466c02a3d68b975
Vaisha16 <vaishnavi.saravana01@gmail.com>
2021-09-24 12:47:51 +0530

    Pattern NO 11 : Dynamic values to print the pattern

--- MyLearnings/src/com/myLearning/basics/Pattern11.java
+++ MyLearnings/src/com/myLearning/basics/Pattern11.java
@@ -1,27 +1,41 @@
 package com.myLearning.basics;
+
+import java.util.Scanner;
+
 public class Pattern11 {
 	public static void main(String arg[]){
-		for(int i=0;i<4;i++)
-		{   int a=i;
-			for(int j=0;j<4;j++)
-			{	
-				if(i==3 && j==3)
-					System.out.println(1);
-				else if(i==0||j==0||j==3)
-					{
-					a++;
-					System.out.print(a+"\t");
-					}
-				else if(i==3)
-					{a--;
-					System.out.print(a+"\t");
-					}
-				else
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter the Pattern Size: ");
+		int size=scan.nextInt();
+		int a,b=size;
+		for(int i=0;i<size;i++){   
+			a=i;
+			if(i==0) {
+				for(int j=0;j<size;j++){	
+					System.out.print(++a+"\t");
+				}
+			}
+			else if(i==size-1) {
+				a=size;
+				for(int j=size;j>0;j--){	
+					System.out.print(a--+"\t");
+				}
+			}
+			
+			else {
+				a=i;
+				for(int k=i;k<i+1;k++) {
+					System.out.print(++a+"\t");
+				}
+				for(int l=2;l<size;l++) {
 					System.out.print("\t");
+				}
+				for(int m=size;m<size+1;m++) {
+					System.out.print(--b+"\t");
+				}
+				
 			}
 			System.out.println("");
-			//System.out.println("");
-			//System.out.println("");
 		}
 	} 
 }
