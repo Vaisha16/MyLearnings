bc06b0c04cb8e1c2bb4db0753be10918c3e677a2
Vaisha16 <vaishnavi.saravana01@gmail.com>
2021-09-27 11:47:29 +0530

    to check whether the given input can be obtained by adding the elements of array but in consecutive order

--- MyLearnings/src/com/myLearning/arrayBasic/SubArraySum.java
+++ MyLearnings/src/com/myLearning/arrayBasic/SubArraySum.java
@@ -1,18 +1,43 @@
 package com.myLearning.arrayBasic;
 
+import java.util.Scanner;
+
 public class SubArraySum {
-	public static void main(String args[]) {
-		int a[]= {1,2,3,4,6,9};
-		int size=a.length,sum=0;
-		int first=0,last=0,s=5;
+	int first=0,last=0,s,sum=0;
+	
+	void toCheck(int a[],int size) {
 		for(int i=0;i<size;i++) {
-			first=i;
 			sum+=a[i];
-			while(sum>=s) {
-				sum-=a[first];
-				first++;
-			}last=i;
-			System.out.println("Position from: "+first+" to "+last);
+			if(sum>=s) {
+				last=i;
+				while(s<sum && first<last) {
+					sum-=a[first];
+					++first;
+			}
+			if(sum==s) {
+					System.out.println("From position "+(first+1)+" to position "+(last+1));
+					break;
+				}
+			else {
+				System.out.println("no elements");
+				break;
+				}
+			}
+			
 		}
+	}
+	
+	public static void main(String args[]) {
+		SubArraySum subarray=new SubArraySum();
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the array size: ");
+		int size=scanner.nextInt();
+		int[] a=new int[size];
+		System.out.println("Enter the array Elements: ");
+		for(int i=0;i<size;i++)
+			a[i]=scanner.nextInt();
+		System.out.println("Enter the sum value: ");
+		subarray.s=scanner.nextInt();
+		subarray.toCheck(a, size);
 	}
 }
