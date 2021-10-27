c08ee8b4065bdc4c4775e2df63d4d712a86b32ec
Vaisha16 <vaishnavi.saravana01@gmail.com>
2021-10-27 12:58:07 +0530

    Union intersection

--- MyLearnings/src/com/myLearning/arrayBasic/UnionIntersection.java
+++ MyLearnings/src/com/myLearning/arrayBasic/UnionIntersection.java
@@ -1,33 +1,62 @@
 package com.myLearning.arrayBasic;
 
-import java.util.Scanner;
-
 public class UnionIntersection {
-	int temp=0;
-	void getPeak(int size1,int[] a,int[] b,int size2){	
-		for(int i=0;i<size1;i++) {
-			if(a[i]>temp) {
-				temp=a[i];
-				}
-			}System.out.println(temp);
-		}
+	int a[]= {1,2,3};
+	int b[]= {1,2,3,4,5};
+	int c[]=new int[(a.length+b.length)];
+	int len=a.length;
+	boolean duplicate;
+	int lastIndex=0;
 	
-
+	void printUnion() {
+		int c[]=new int[(a.length+b.length)];
+		int lastIndex=0;
+		for(int i=0;i<a.length;i++)
+			c[i]=a[i];
+		
+		for(int i=0;i<b.length;i++) {
+			duplicate=false;
+			for(int j=0;j<a.length;j++) {
+				if(b[i]==c[j]) {
+					duplicate=true;
+					break;
+				}
+			}if(!duplicate) {
+				c[len]=b[i];
+				len++;
+				lastIndex=len;
+			}
+		}
+		System.out.print("Union: ");
+		for(int i=0;i<lastIndex;i++) {
+			System.out.print(c[i]+"\t");
+		}
+	}
+	
+	void printIntersection() {
+		len=0;
+		for(int i=0;i<b.length;i++) {
+			duplicate=false;
+			for(int j=0;j<a.length;j++) {
+				if(b[i]==a[j]) {
+					duplicate=true;
+					break;
+				}
+			}if(duplicate) {
+				c[len]=b[i];
+				len++;
+				lastIndex=len;
+			}
+		}
+		System.out.print("Intersection: ");
+		for(int i=0;i<lastIndex;i++) {
+			System.out.print(c[i]+"\t");
+		}
+	}
+	
 	public static void main(String args[]) {
-		UnionIntersection p=new UnionIntersection();
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter the Size of First Array: ");
-		int size1=s.nextInt();
-		int a[]=new int[size1];
-		System.out.println("Enter the Elements of First Array: ");
-		for(int i=0;i<size1;i++)
-			a[i]=s.nextInt();
-		System.out.println("Enter the Size of Second Array: ");
-		int size2=s.nextInt();
-		int b[]=new int[size2];
-		System.out.println("Enter the Elements of Second Array: ");
-		for(int i=0;i<size2;i++)
-			b[i]=s.nextInt();
-		p.getPeak(size1,a,b,size2);
+		UnionIntersection u=new UnionIntersection();
+		u.printUnion();
+		u.printIntersection();
 	}
 }
